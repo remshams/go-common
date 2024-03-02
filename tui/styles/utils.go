3869d22ba6c01ca9a31ef9498c3aa8a136d31a5c
Mathias Remshardt <remshams@gmail.com>
2024-03-02 19:41:43 +0100

    feat(tui-Styles): updates method for calculating dimensions
    Now accepts a max value to limit the calculated absolute value

--- tui/styles/utils.go
+++ tui/styles/utils.go
@@ -2,6 +2,10 @@ package styles
 
 import "math"
 
-func CalculateDimensionsFromPercentage(percent int, total int) int {
-	return int(math.Round(float64(total) * (float64(percent) / 100)))
+func CalculateDimensionsFromPercentage(percent int, total int, max *int) int {
+	dimensions := int(math.Round(float64(total) * (float64(percent) / 100)))
+	if max != nil && dimensions > *max {
+		dimensions = *max
+	}
+	return dimensions
 }
---
+++ tui/styles/utils_test.go
@@ -0,0 +1,23 @@
+package styles
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateDimensionsFromPercentage_ShouldReturnPercentage(t *testing.T) {
+	percent := 50
+	total := 100
+	absolute := 50
+	dimensions := CalculateDimensionsFromPercentage(percent, total, nil)
+	assert.Equal(t, absolute, dimensions)
+}
+
+func TestCalculateDimensionsFromPercentage_ShouldReturnMax(t *testing.T) {
+	percent := 50
+	total := 100
+	max := 25
+	dimensions := CalculateDimensionsFromPercentage(percent, total, &max)
+	assert.Equal(t, max, dimensions)
+}
